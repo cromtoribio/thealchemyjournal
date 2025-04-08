cda03cd21693e5f9c9904cb1448c32c31355807a
Chris Toribio <44413949+cromtoribio@users.noreply.github.com>
2025-04-07 21:29:56 -0700

    Quartz sync: Apr 7, 2025, 9:29 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "journal.christoribio.com",
+    baseUrl: "cromtoribio.github.io",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
